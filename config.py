296cb57d168ab0471050716e3d7cc500480045d2
onazemaq90 <onazemaq90@hotmail.com>
2025-03-13 01:48:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID", ))
-    API_HASH = os.getenv("API_HASH", '')
-    BOT_TOKEN = os.getenv("BOT_TOKEN", '')
+    API_ID = int(os.getenv("API_ID", 26489431))
+    API_HASH = os.getenv("API_HASH", '9a2fce85339bb79254a55368a61ab92f')
+    BOT_TOKEN = os.getenv("BOT_TOKEN", '7506131429:AAE8AqZ8uAMIj8La9kluJLQUeGvVJaQIlzM')
